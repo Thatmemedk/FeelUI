6b0def86840df2f3fd5df65c838cb11c82ab29f3
Thatmemedk <guillaumeellby@gmail.com>
2026-08-15 18:30:23 +0200

    Remove that

--- Modules/CooldownFrame/Utils.lua
+++ Modules/CooldownFrame/Utils.lua
@@ -42,27 +42,6 @@ function UI:GetCooldownFontScale(CD)
     return FontSize
 end
 
-function UI:BuildRuleDurationFormatter()
-    local Formatter = C_StringUtil.CreateNumericRuleFormatter()
-    local Down = Enum.NumericRuleFormatRounding.Down
-    local ExpireColor = CreateColor(unpack(DB.Global.CooldownFrame.ExpireColor))
-    local SecondsColor = CreateColor(unpack(DB.Global.CooldownFrame.SecondsColor))
-    local SecondsColor2 = CreateColor(unpack(DB.Global.CooldownFrame.SecondsColor2))
-    local NormalColor = CreateColor(unpack(DB.Global.CooldownFrame.NormalColor))
-
-    Formatter:SetBreakpoints({
-        { threshold = 0, format = ExpireColor:WrapTextInColorCode("%.1f"), step = 0.1, rounding = Down },
-        { threshold = 10, format = SecondsColor:WrapTextInColorCode("%d"), step = 1, rounding = Down },
-        { threshold = 30, format = SecondsColor2:WrapTextInColorCode("%d"), step = 1, rounding = Down },
-        { threshold = 60, format = NormalColor:WrapTextInColorCode("%d:%02d"), step = 1, rounding = Down, components = {{ div = 60 }, { div = 1, mod = 60 }} },
-        { threshold = 120, format = NormalColor:WrapTextInColorCode("%dm"), step = 1, rounding = Down, components = {{ div = 60 }} },
-        { threshold = 3600, format = NormalColor:WrapTextInColorCode("%dh"), step = 1, rounding = Down, components = {{ div = 3600 }} },
-        { threshold = 86400, format = NormalColor:WrapTextInColorCode("%dd"), step = 1, rounding = Down, components = {{ div = 86400 }} },
-    })
-
-    return Formatter
-end
-
 function UI:UpdateCooldownText(CD, Parent, OffsetX, OffsetY, DynamicFontSize)
     if (not CD or CD.CooldownTextIsUpdated or CD:IsForbidden() or UI:IsSecretValue(CD)) then
         return
